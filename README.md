694809853d7f2fb5419ff2556addc8b64ea35c82
Purple Persia <navid@navids-imac.mynetworksettings.com>
2024-07-12 08:18:41 -0400

    added modal, form, and js to click

---
+++ README.md
@@ -0,0 +1,15 @@
+#kanban
+
+Description:
+We were tasked in our BootCamp to create a task baord from starter code using HTML, CSS, & JS.
+
+Usage:
+To use the skills we've learned in class to build a kanbanboard from starter code using our current knowledge.
+
+![kanban](image.png)
+
+Credits:
+W3Schools MDN Web Docs freecodecamp.org
+
+License:
+N/A
---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+// Retrieve tasks and nextId from localStorage
+let taskList = JSON.parse(localStorage.getItem("tasks"));
+let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+// Todo: create a function to generate a unique task id
+function generateTaskId() {
+
+}
+
+// Todo: create a function to create a task card
+function createTaskCard(task) {
+
+}
+
+// Todo: create a function to render the task list and make cards draggable
+function renderTaskList() {
+
+}
+
+// Todo: create a function to handle adding a new task
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-success");
+const closeModalBtn = document.querySelector(".btn-close");
+
+// close modal function
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// close the modal when the close button and overlay is clicked
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+// close modal when the Esc key is pressed
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+// open modal function
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+// open modal event
+openModalBtn.addEventListener("click", openModal);
